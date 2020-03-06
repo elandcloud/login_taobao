e6af2de7cc82a8e445d422ae7037740b92222b27
xiao.xinmiao <xiaoxm_001@163.com>
2020-03-06 08:12:21 +0800

    #5 refactoring request param

--- main.py
+++ main.py
@@ -18,34 +18,20 @@ cookies过期，删除cookies文件！
 class main:
     def __init__(self):
         pass
-    def login(self):
-        # 淘宝用户名
-        username = os.getenv("T_NAME")
-        # 淘宝重要参数，从浏览器或抓包工具中复制，可重复使用
-        ua = os.getenv("T_UA")
-        # 加密后的密码，从浏览器或抓包工具中复制，可重复使用
-        TPL_password2 = os.getenv("T_PWD2")
-        ul = login.UsernameLogin(username, ua, TPL_password2)
-        err=ul.login()
-        return err
     def start(self):
-        # err=gui_.Gui().reqUrl("https://taobao.com")
-        # if err !=None:
-        #     return err
         header,param,err =gui_.Gui().copyHTMLPostLoop(name="login.jhtml")
         if err !=None:
             return err
         err=file_.write(utils.DATA_PATH,utils.DATA_PATH_HEADER,header)
         if err !=None:
             return err
-        param=utils.regexSub(param,"TPL_username=\n","TPL_username=")
-        param=utils.regexSub(param,"TPL_password_2=\n","TPL_password_2=")
-        param=utils.regexSub(param,"ua=\n","ua=")
 
         err=file_.write(utils.DATA_PATH,utils.DATA_PATH_PARAM,param)
         if err !=None:
             return err
-        err=main().login()
+        data=req.Req().readParamPost(param)
+        ul = login.UsernameLogin(data["TPL_username"], data["ua"], data["TPL_password_2"])
+        err=ul.login()
         if err !=None:
             return err
 if __name__ == '__main__':
